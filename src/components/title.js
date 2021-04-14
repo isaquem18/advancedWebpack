3fe7d95512e1ae8e53f6938df3e248fabef8b4bd
isaque <isaquem18@gmail.com>
2021-04-14 03:00:14 -0300

    loader#2

--- src/components/title.js
+++ src/components/title.js
@@ -1,4 +1,8 @@
 import './title.css';
+import './parag.scss';
+import monkey from '../assets/monk.jpg'; 
+
+//const { resolve } = require('path');
 
 class Title {
     create(text) {
@@ -9,6 +13,15 @@ class Title {
         const image = document.createElement('img');
         image.setAttribute('src', 'https://www.bambui.ifmg.edu.br/portal_padrao_joomla/joomla/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png');
         document.querySelector('body').appendChild(image);
+
+        const image2 = document.createElement('img');
+        image2.setAttribute('src', monkey);
+        document.querySelector('body').appendChild(image2);
+
+
+        const lorem = document.createElement('p');
+        lorem.innerHTML = 'Lorem ipsum dolor sit amet, consectetur <span>adipisicing elit. Mollitia</span> nihil quas impedit fuga quisquam! Sed cum minus ';
+        document.querySelector('body').appendChild(lorem);
     }
 }
 
